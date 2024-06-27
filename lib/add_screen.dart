d4a549604e66d073325f2b9f33b9807c3d48e215
JingwenHuang233 <jingwenhuang233@gmail.com>
2024-06-26 22:12:54 -0700

    add upload options to add_video_screen

--- lib/add_screen.dart
+++ lib/add_screen.dart
@@ -3,15 +3,67 @@ import 'package:temp_flutter/constants.dart';
 
 class AddVideo extends StatelessWidget {
   const AddVideo({super.key});
-  // showOptions(BuildContext context){
-  //
-  // }
+  showOptions(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (context) => SimpleDialog(
+        children: [
+          SimpleDialogOption(
+            onPressed: () {},
+            child: const Row(
+              children: [
+                Icon(Icons.image),
+                Padding(
+                  padding: EdgeInsets.all(7.0),
+                  child: Text(
+                    'Gallery',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SimpleDialogOption(
+            onPressed: () {},
+            child: const Row(
+              children: [
+                Icon(Icons.camera_alt),
+                Padding(
+                  padding: EdgeInsets.all(7.0),
+                  child: Text(
+                    'Camera',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SimpleDialogOption(
+            onPressed: () => Navigator.of(context).pop(),
+            child: const Row(
+              children: [
+                Icon(Icons.cancel),
+                Padding(
+                  padding: EdgeInsets.all(7.0),
+                  child: Text(
+                    'Cancel',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: InkWell(
-          // onTap: () => showOptions,
+          onTap: () => showOptions(context),
           child: Container(
             width: 190,
             height: 50,
