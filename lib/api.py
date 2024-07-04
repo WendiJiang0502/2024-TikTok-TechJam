877774483704f13a22788978c8db6d3b953a2903
WendiJiang0502 <142946611+WendiJiang0502@users.noreply.github.com>
2024-07-04 16:42:49 -0700

    Add files via upload

---
+++ lib/api.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+import json
+from recommender import recommend_songs
+
+app = Flask(__name__)
+
+# Load song data from JSON files
+with open('lib/assets/songs.json', 'r') as f:
+    songs_data = json.load(f)
+
+# Load user data from JSON file
+with open('lib/me.json', 'r') as f:
+    user_data = json.load(f)
+
+@app.route('/recommendations', methods=['GET'])
+def get_recommendations():
+    recommendations = recommend_songs(user_data, songs_data)
+    recommendation_titles = [song['Title'] for song in recommendations]
+    return jsonify(recommendation_titles)
+
+if __name__ == "__main__":
+    app.run(debug=True)
