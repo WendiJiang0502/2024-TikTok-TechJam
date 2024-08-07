5a4bdd670fc73dd660ca16c01e831834d7490680
JingwenHuang233 <jingwenhuang233@gmail.com>
2024-08-07 00:21:25 -0700

    implement upload feature with example videos

--- lib/add_screen.dart
+++ lib/add_screen.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:temp_flutter/constants.dart';
 import 'package:temp_flutter/music_select_page.dart';
-import 'dart:io';
-import 'package:flutter/services.dart';
-import 'package:path_provider/path_provider.dart';
+
 
 class AddVideo extends StatelessWidget {
   final ImagePicker _picker = ImagePicker();
@@ -48,6 +46,21 @@ class AddVideo extends StatelessWidget {
             ),
           ),
           SimpleDialogOption(
+            onPressed: () => showAssetOptions(context),
+            child: const Row(
+              children: [
+                Icon(Icons.video_library),
+                Padding(
+                  padding: EdgeInsets.all(7.0),
+                  child: Text(
+                    'Select from Assets',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SimpleDialogOption(
             onPressed: () => Navigator.of(context).pop(),
             child: const Row(
               children: [
@@ -67,39 +80,70 @@ class AddVideo extends StatelessWidget {
     );
   }
 
+  void showAssetOptions(BuildContext context) {
+    final List<String> assetVideos = [
+      'lib/assets/Magical_Night_Street.mp4',
+      'lib/assets/1つのシーンで作る日本食ビデオ！.mp4',
+      'lib/assets/travel_video.mp4',
+    ];
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        String? selectedAsset;
+        return AlertDialog(
+          title: const Text('Select an Asset Video'),
+          content: DropdownButton<String>(
+            isExpanded: true,
+            hint: const Text('Select a video'),
+            value: selectedAsset,
+            items: assetVideos.map((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+            onChanged: (value) {
+              selectedAsset = value;
+              Navigator.of(context).pop();
+              final musicPath = Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => MusicSelectionScreen(videoPath: selectedAsset!),
+                ),
+              );
+              if (musicPath != null) {
+                print("Video path: $selectedAsset, Music path: $musicPath");
+                // Navigator.push(
+                //   context,
+                //   MaterialPageRoute(
+                //     builder: (context) => ShortVideoDisplay(
+                //       videoPath: selectedAsset!,
+                //       bgmPath: musicPath as String?,
+                //     ),
+                //   ),
+                // );
+              }
+            },
+          ),
+        );
+      },
+    );
+  }
+
   void pickVideo(BuildContext context, ImageSource source) async {
-    // final XFile? video = await _picker.pickVideo(source: source);
-    // if (video != null) {
-    //   Navigator.of(context).pop();  // Close the dialog after selection
-    //   final musicPath = await Navigator.push(
-    //     context,
-    //     MaterialPageRoute(
-    //       builder: (context) => MusicSelectionScreen(videoPath: video.path),
-    //     ),
-    //   );
-    //   if (musicPath != null) {
-    //     // Now you have both the video path and the selected music path
-    //     print("Video path: ${video.path}, Music path: $musicPath");
-    //     // Proceed to overlay music on video here
-    //   }
-    // }
     final XFile? video = await _picker.pickVideo(source: source);
     if (video != null) {
-      final Directory appDocDir = await getApplicationDocumentsDirectory();
-      final String appDocPath = appDocDir.path;
-      final String newVideoPath = '$appDocPath/${video.name}';
-      await File(video.path).copy(newVideoPath);
-
-      Navigator.of(context).pop(); // Close the dialog after selection
+      Navigator.of(context).pop();  // Close the dialog after selection
       final musicPath = await Navigator.push(
         context,
         MaterialPageRoute(
-          builder: (context) => MusicSelectionScreen(videoPath: newVideoPath),
+          builder: (context) => MusicSelectionScreen(videoPath: video.path),
         ),
       );
       if (musicPath != null) {
         // Now you have both the video path and the selected music path
-        print("Video path: $newVideoPath, Music path: $musicPath");
+        print("Video path: ${video.path}, Music path: $musicPath");
         // Proceed to overlay music on video here
       }
     }
